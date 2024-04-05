f660da9141798d200bace7f32c0cfada04fbc509
aivantg <aivant.developer@gmail.com>
2024-04-05 02:03:51 -0700

    Update setup script

--- setup_pi.sh
+++ setup_pi.sh
@@ -1,13 +1,18 @@
+## Assumes you've just run the following: 
+# git clone https://github.com/aivantg/scavenger-hunt-audio-server.git
+# cd scavenger-hunt-audio-server
+# source setup_pi.sh
+
 sudo apt-get update && sudo apt-get upgrade -y
 
 # Install basic packages 
-sudo apt install ufw git mosh speedtest-cli  -y 
+sudo apt install ufw git mosh python3-flask python3-pygame -y 
 
 # Allow password login
 # Enter new password manually
 sudo passwd
-sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sudo systemctl restart sshd
+# Uncomment line "PasswordAuthentication yes" in /etc/ssh/sshd_config automatically
+sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 
 # Setup firewall
 sudo ufw enable
@@ -15,22 +20,18 @@ sudo ufw allow ssh
 sudo ufw allow 60000:61000/udp
 sudo ufw allow 5000/tcp
 
-# Install python dependencies
-sudo apt install python3-flask
-sudo apt install python3-pygame
-
-# Clone this repo
-git clone https://github.com/aivantg/scavenger-hunt-audio-server.git
-cd scavenger-hunt-audio-server
-
 # Copy music server to home directory
 cp -R music_server ..
 
 # Set and save volume settings
-amixer -c 2 set PCM 100%
+# may need to adjust to ensure correct sound card is being used
+# `sudo aplay -l` shows sound card options, replace '2' with correct card number
+# `sudo alsamixer` shows UI for adjusting volume. Match `PCM` with the name of the volume control. 
+# Either 'PCM' or 'Master' are common.
+amixer -c 2 set PCM 100% 
 sudo alsactl store
 
-# Set up system services
+# Set up music server to start on boot
 sudo cp setup/alsa_restore.service /etc/systemd/system/alsa_restore.service
 sudo cp setup/music_server.service /etc/systemd/system/music_server.service
 sudo systemctl daemon-reload
@@ -38,9 +39,7 @@ sudo systemctl enable alsa_restore
 sudo systemctl enable music_server
 
 # Copy over wifi settings 
-# Add wifi credentials
 sudo cp setup/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
-sudo systemctl restart networking
 
 # Reboot
 sudo systemctl reboot
